22fa49768d1c2b509f990bd1db1a8d76bf26c6d5
Lamine Kama <laminekama038@gmail.com>
2025-06-21 06:45:54 +0800

    btn message

--- back-end/app/Http/Controllers/chatController.php
+++ back-end/app/Http/Controllers/chatController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ChatBot;
 use Illuminate\Http\Request;
 
 class chatController extends Controller
@@ -13,11 +14,17 @@ class chatController extends Controller
 
         $reply = match (true) {
             str_contains($message, 'bonjour') => 'Salut ! Comment puis-je vous aider ?',
-            str_contains($message, 'merci') => 'Avec plaisir !',
+            str_contains($message, 'comment ça va') => 'Je vais bien, merci ! Et vous ?',
+            str_contains($message, 'je veux des informations') => 'Voici les informations que vous avez demandées.',
+            str_contains($message, 'merci') => 'De rien ! Si vous avez d’autres questions, n’hésitez pas.',
+            str_contains($message, 'd\'accord') => 'Avec plaisir !',
             str_contains($message, 'aide') => 'Je suis là pour vous aider. Posez votre question.',
             default => "Désolé, je n'ai pas compris.",
         };
-
+        ChatBot::create([
+            'message_user' => $message,
+            'reponse_bot' => $reply,
+        ]);
         return response()->json(['reply' => $reply]);
     }
 }
